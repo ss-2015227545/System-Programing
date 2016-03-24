46e4576369db5b624aff35fa780c387c71a23f47
ss-2015227545 <halove2002@gmail.com>
2016-03-24 15:34:41 +0900

    [2번째 과제 업로드]

---
+++ 2015227545_PE2.c
@@ -0,0 +1,154 @@
+#include <sys/types.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <pwd.h>
+#include <grp.h>
+#include <stdio.h>
+#include <string.h>
+
+void do_ls(char[]);
+void dostat(char*);
+void show_file_info(char*, struct stat*);
+void mode_to_letters(int, char[]);
+char *uid_to_name(uid_t);
+char *gid_to_name(gid_t);
+void rels(char[]);
+
+char next[50] = ".";
+char temp[50] = {};
+char dirpath[50] = {};
+
+void main(int ac, char* av[])
+{
+        if(ac == 1)
+                do_ls(".");
+        else if(ac == 2)
+        {
+                if(((strcmp("-R", av[1])) == 0)){
+                        rels(".");
+                }
+        }
+        else
+                while( --ac){
+                printf("%s:\n", * ++av);
+                do_ls(*av);
+                }
+}
+
+void rels(char *path){
+        DIR *dir;
+        struct dirent *dirslot;
+        struct stat info;
+        chdir(path);
+        dir=opendir(".");
+        printf("%s:\n",dirpath);
+        while (1) {
+                dirslot=readdir(dir);
+                if (dirslot==NULL) break;
+                stat(dirslot->d_name, &info);
+                show_file_info(dirslot->d_name,&info);
+        }
+        rewinddir(dir);
+        while (1) {
+                dirslot=readdir(dir);
+                if (dirslot==NULL) return;
+                if (strcmp(dirslot->d_name, ".")==0) continue;
+                if (strcmp(dirslot->d_name, "..")==0) continue;
+                stat(dirslot->d_name, &info);
+                if (S_ISDIR(info.st_mode))  {
+                        printf("\n\n");
+                        strcpy(temp, next);
+                        strcpy(dirpath,temp);
+                        strcat(dirpath,"/");
+                        strcat(next,dirslot->d_name);
+                        strcat(dirpath, dirslot->d_name);
+                        chdir(dirslot->d_name);
+                        rels(dirslot->d_name);
+                        chdir("..");
+                        strcpy(next,temp);
+                        }
+                }
+}
+
+
+void do_ls(char dirname[]){
+        DIR *dir_ptr;
+        struct dirent *direntp;
+
+        if((dir_ptr = opendir(dirname))==NULL)
+        fprintf(stderr, "lsl : cannot open %s\n", dirname);
+        else {
+                while((direntp = readdir(dir_ptr))!=NULL)
+            	    dostat(direntp->d_name);
+        closedir(dir_ptr);
+        }
+}
+
+void dostat(char *filename){
+        struct stat info;
+        if( stat(filename, &info) == -1)
+                perror(filename);
+        else
+                show_file_info( filename, &info);
+}
+
+void show_file_info( char * filename, struct stat *info_p ){
+        char *uid_to_name(), *ctime(), *gid_to_name(), *filemode();
+        void mode_to_letters();
+        char modestr[11];
+
+        mode_to_letters(info_p -> st_mode, modestr);
+
+        printf("%s ",modestr);
+        printf("%4d ",(int) info_p->st_nlink);
+        printf("%-8s ",uid_to_name(info_p->st_uid));
+        printf("%-8s ",gid_to_name(info_p->st_gid));
+        printf("%8ld ",(long)info_p->st_size);
+        printf("%.12s ",4+ctime(&info_p->st_mtime));
+        printf("%s\n",filename);
+}
+
+void mode_to_letters(int mode, char str[]){
+        strcpy(str, "----------");
+        if(S_ISDIR(mode)) str[0] ='d';
+        if(S_ISCHR(mode)) str[0] ='c';
+        if(S_ISBLK(mode)) str[0] ='b';
+
+        if(mode & S_IRUSR) str[1] = 'r';
+        if(mode & S_IWUSR) str[2] = 'w';
+        if(mode & S_IXUSR) str[3] = 'x';
+
+        if(mode & S_IRGRP) str[4] = 'r';
+        if(mode & S_IWGRP) str[5] = 'w';
+        if(mode & S_IXGRP) str[6] = 'x';
+
+        if(mode & S_IROTH) str[7] = 'r';
+        if(mode & S_IWOTH) str[8] = 'w';
+        if(mode & S_IXOTH) str[9] = 'x';
+
+}
+
+char *uid_to_name(uid_t uid){
+        struct passwd *getpwuid(), *pw_ptr;
+        static char numstr[10];
+
+
+        if(( pw_ptr = getpwuid(uid))==NULL){
+        sprintf(numstr, "%d", uid);
+        return numstr;
+        }
+        else
+                return pw_ptr->pw_name;
+}
+
+char *gid_to_name(gid_t gid){
+        struct group *getgrgid(), *grp_ptr;
+        static char numstr[10];
+        if(( grp_ptr = getgrgid(gid))==NULL){
+        sprintf(numstr, "%d", gid);
+        return numstr;
+        }
+        else
+                return grp_ptr->gr_name;
+}
+
